73ce4e993e308a09f4cd6f6e12e537ecf008f5e8
Prashant Bhardwaj <geekyprashant840@gmail.com>
2023-04-06 16:11:24 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Virag Kormoczy",
+    job: "MERN Stack Developer",
+    image: "/images/avatar1.jpg",
+    text: "I am pleased to recommend Prashant for his exceptional skills as a front-end developer, particularly with React. Prashant consistently demonstrated his ability to create visually stunning, highly-functional user interfaces that delighted users. He has a keen eye for design and a deep understanding of React, which allows him to implement complex features with ease.",
+  },
+  {
+    id: 2,
+    name: "Rakesh Mishra",
+    job: "Recruitment Leader at Amelia",
+    image: "/images/avatar2.jpg",
+    text: "Prashant is smart, collaborative fast engineer. He is ideal for small, early stage companies since he works hard and produces solid product quickly. He will adapt his work ethic smoothly to nearly any sensible process. I would recommend and vouch him to any Organization as he is definitely an assets to any Org.",
+  },
+  {
+    id: 3,
+    name: "Rory Bernier",
+    job: "Director of BD at FourBends",
+    image: "/images/avatar3.jpg",
+    text: "I had the pleasure of working with Prashant on a frontend development project, and I can confidently say that he is an exceptional developer. Prashant consistently delivered high-quality work on time and was always willing to go above and beyond to ensure that our project was a success. ",
+  },
+  {
+    id: 4,
+    name: "Ben ali issam Eddine",
+    job: "Mechatronic Engineer",
+    image: "/images/avatar4.jpg",
+    text: "Strongly recommended Prashant Bhardwaj for his powerfull skills : his coding was excellent and his designs were unique and innovative,He was always thoughtful and considerate, an excellent team player who can also work independently and do a fantastic job. Prashant can seemingly do anything, so he’s very highly recommended",
+  },
+  {
+    id: 5,
+    name: "Engr Promise Uka",
+    job: "Full Stack Developer",
+    image: "/images/avatar5.jpg",
+    text: "Prashant is a great developer. I have followed him closely here on LinkedIn and I have seen how passionate he is towards improving his skills as a developer. Prashant is an amazing technical writer also and fine problem solver. If you are looking for someone who is passionate and dedicated to delivering world class web application then you can try Prashant and I can assure you that he's going to deliver.",
+  },
+];
+
+export default reviews;
